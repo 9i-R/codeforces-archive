3b9583dc46c0dc8275d8dfc0b008d3e3c175a5d3
9eplicant. <keizhatp@disroot.org>
2025-09-22 22:38:50 +0700

    Create 50A.cpp

---
+++ 50A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int M, N;
+ 
+int main() {
+  ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+  
+	cin>>M>>N;
+	cout<<M*N/2<<endl;
+
+  return 0;
+}
